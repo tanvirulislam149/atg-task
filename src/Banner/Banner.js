2279dc779519a34048c5d5100481e58308c2a244
tanvir <tanvirulislam149@gmail.com>
2022-07-27 14:41:48 +0600

    banner done

---
+++ src/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+   return (
+      <div className="banner-background">
+         <div className="text-white banner-text">
+            <p className="fw-bold fs-1 mb-0">Computer Engineering</p>
+            <p>142,765 Computer Engineers follow this</p>
+         </div>
+      </div>
+   );
+};
+
+export default Banner;
